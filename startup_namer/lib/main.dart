4518c11a317630a5ab27a7560a9abae3042dce9b
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2021-02-05 17:25:43 +0300

    Adding interactivity to your Flutter app
    Adding interactivity to your Flutter app
    src https://flutter.dev/docs/development/ui/interactive

--- startup_namer/lib/main.dart
+++ startup_namer/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
@@ -8,58 +9,63 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  Widget titleSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Row(
-      children: [
-        Expanded(
-          /*1*/
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              /*2*/
-              Container(
-                padding: const EdgeInsets.only(bottom: 8),
-                child: Text(
-                  'Lake Naivasha Campground',
-                  style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-              ),
-              Text(
-                'Naivasha, Kenya',
-                style: TextStyle(
-                  color: Colors.grey[500],
-                ),
-              ),
-            ],
-          ),
-        ),
-        /*3*/
-        Icon(
-          Icons.star,
-          color: Colors.red[500],
-        ),
-        Text('41'),
-      ],
-    ),
-  );
-  Widget textSection = Container(
-    padding: const EdgeInsets.all(32),
-    child: Text(
-      'Lake Naivasha is a freshwater lake in Kenya '
-      'outside the town of Naivasha in Nakuru County, which lies north west of Nairobi. '
-      'It is part of the Great Rift Valley.'
-      'The name derives from the local Maasai name Naiposha, '
-      'meaning "rough water" because of the sudden storms which can arise.',
-      softWrap: true,
-    ),
-  );
-
   @override
   Widget build(BuildContext context) {
+    Widget titleSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Container(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    'Lake Naivasha Campground',
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                ),
+                Text(
+                  'Naivasha, Kenya',
+                  style: TextStyle(
+                    color: Colors.grey[500],
+                  ),
+                ),
+              ],
+            ),
+          ),
+          /*3*/
+          FavoriteWidget(),
+        ],
+      ),
+    );
+    Widget textSection = Container(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        'Lake Naivasha is a freshwater lake in Kenya '
+        'outside the town of Naivasha in Nakuru County, which lies north west of Nairobi. '
+        'It is part of the Great Rift Valley.'
+        'The name derives from the local Maasai name Naiposha, '
+        'meaning "rough water" because of the sudden storms which can arise.',
+        softWrap: true,
+      ),
+    );
     Color color = Theme.of(context).primaryColor;
+    Widget buttonSection = Container(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          _buildButtonColumn(color, Icons.call, 'CALL'),
+          _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
+          _buildButtonColumn(color, Icons.share, 'SHARE'),
+        ],
+      ),
+    );
     return MaterialApp(
       title: 'Flutter Layout Demo',
       home: Scaffold(
@@ -75,16 +81,7 @@ class _MyAppState extends State<MyApp> {
               fit: BoxFit.cover,
             ),
             titleSection,
-            Container(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: <Widget>[
-                  _buildButtonColumn(color, Icons.call, 'CALL'),
-                  _buildButtonColumn(color, Icons.near_me, 'ROUTE'),
-                  _buildButtonColumn(color, Icons.share, 'SHARE'),
-                ],
-              ),
-            ),
+            buttonSection,
             textSection
           ],
         ),
@@ -111,5 +108,52 @@ class _MyAppState extends State<MyApp> {
         )
       ],
     );
+  }
+}
+
+class FavoriteWidget extends StatefulWidget {
+  @override
+  _FavoriteWidgetState createState() => _FavoriteWidgetState();
+}
+
+class _FavoriteWidgetState extends State<FavoriteWidget> {
+  bool _isFavorited = true;
+  int _favoriteCount = 41;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.min,
+      children: <Widget>[
+        Container(
+          padding: EdgeInsets.all(0),
+          child: IconButton(
+              icon: (_isFavorited ? Icon(Icons.star) : Icon(Icons.star_border)),
+              onPressed: _toggleFavorite,
+            padding: EdgeInsets.all(0),
+            color: Colors.red[500],
+            alignment: Alignment.center,
+          ),
+        ),
+        SizedBox(
+          width: 18,
+          child: Container(
+            child: Text('$_favoriteCount'),
+          ),
+        )
+      ],
+    );
+  }
+
+  void _toggleFavorite() {
+    setState(() {
+      if (_isFavorited) {
+        _favoriteCount -= 1;
+        _isFavorited = false;
+      } else {
+        _favoriteCount += 1;
+        _isFavorited = true;
+      }
+    });
   }
 }
