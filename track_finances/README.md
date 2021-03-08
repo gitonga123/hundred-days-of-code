0d9780e485a57e14ed43d521409cb3282c91f0c9
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2021-03-08 21:07:51 +0300

    Initial commit

--- track_finances/README.md
+++ track_finances/README.md
@@ -1,6 +1,6 @@
 # track_finances
 
-A new Flutter project.
+A new Flutter application.
 
 ## Getting Started
 
--- track_finances/lib/models/received_money.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class Received {
-  final String group;
-  final double value;
-  final Color color;
-
-  const Received(this.group, this.value, this.color);
-}
