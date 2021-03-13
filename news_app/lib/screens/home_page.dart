a6e0bdeb5706f159b2d576af735ae468d9fe3f8e
Daniel Mutwiri <mutwiri.daniel@yahoo.com>
2021-03-13 04:38:08 +0300

    News List
    Using ListBuilder Seprated, build the list of news item, and display on a expanded ListView

--- news_app/lib/screens/home_page.dart
+++ news_app/lib/screens/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/screens/news_list.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
@@ -13,6 +14,7 @@ class MyHomePage extends StatelessWidget {
         centerTitle: true,
         elevation: 2,
       ),
+      body: NewsList(),
     );
   }
 }
--- news_app/lib/screens/news_list.dart
+++ news_app/lib/screens/news_list.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 import 'dart:convert';
@@ -14,20 +15,64 @@ class _NewsListState extends State<NewsList> {
   static List<News> _newsInApp = List<News>.empty(growable: true);
 
   Future<List<News>> comingNews() async {
-    var url = 'http://www.mocky.io/v2/5ecfddf13200006600e3d6d0';
+    var url = Uri.parse("http://www.mocky.io/v2/5ecfddf13200006600e3d6d0");
     var response = await http.get(url);
-    var news = List<News>();
+    var news = List<News>.empty(growable: true);
     if (response.statusCode == 200) {
       var newsJson = json.decode(response.body);
       for (var n in newsJson) {
-        news.add(News.fromJson(n);
+        news.add(News.fromJson(n));
       }
     }
 
     return news;
   }
+
+  @override
+  void initState() {
+    comingNews().then((value) {
+      setState(() {
+        _news.addAll(value);
+        _newsInApp = _news;
+      });
+    });
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return ListView.separated(
+      padding: const EdgeInsets.all(6),
+      itemBuilder: (context, index) {
+        return _listNews(index);
+      },
+      itemCount: _newsInApp.length,
+      separatorBuilder: (BuildContext context, int index) => const Divider(),
+    );
+  }
+
+  Widget _listNews(index) {
+    return Expanded(
+      child: ListTile(
+        title: Text(
+          _news[index].titles,
+          style: TextStyle(fontWeight: FontWeight.w800, fontSize: 20),
+        ),
+        subtitle: Text(
+          _news[index].author,
+          style: TextStyle(
+              fontWeight: FontWeight.w400,
+              fontStyle: FontStyle.normal,
+              fontSize: 16),
+        ),
+        trailing: IconButton(
+            iconSize: 16,
+            color: Colors.black87,
+            alignment: Alignment.bottomCenter,
+            icon: Icon(Icons.arrow_forward_ios),
+            onPressed: () => print("Am clicked")
+        ),
+      ),
+    );
   }
 }
