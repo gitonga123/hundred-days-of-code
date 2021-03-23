2b2f69a9ee1731574e5ac0b6c25b91168e13521c
mutwiri.daniel@yahoo.com <mutwiridanielsci@gmail.com>
2021-03-23 18:45:41 +0300

    Update - data structure - python, collections in dart

---
+++ dart/arrays.dart
@@ -0,0 +1,7 @@
+import 'dart:math';
+import 'dart:io';
+
+void main(List<String> args) {
+  List<int> favNums = [4, 6, 7, 16, 27, 1, 18, 78];
+  print(favNums[1]);
+}
---
+++ python_ds/binary_tree.py
@@ -0,0 +1,12 @@
+class Node:
+    def __init__(self, data):
+        self.left = None
+        self.right = None
+        self.data = data
+
+
+root = Node(10)
+root.left = Node(34)
+root.right = Node(89)
+
+
